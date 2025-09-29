9def8484f6c02462cafd195a94bad3feab1998e7
Shihab79cdes <shihabmallick02@gmail.com>
2025-09-29 22:32:59 +0530

    Update task.py

--- day 1/task.py
+++ day 1/task.py
@@ -1,1 +1,114 @@
+# DAY 1 (PRINTING , DEBUGGING, STRING MANIPULATION AND VARIABLES)
+
+# print("hello world") - output- hello world
+# print("hello world\nhello world\nhello world") it will print together.No need to write seperate.
+
+# CONCATENATION (means merging)
+
+# print("hello" + "world") result- helloworld
+# print("hello"+" "+"world") result- hello world
+
+
+# commas
+# print("hello","shihab") #output--> hello shihab (commas use for spacing)
+# print("hello" " world")
+# print("hello" " world")
+
+# INPUT FUNCTION
+
+# print("what is your name?")
+# input("what is your name?") asking details from user..
+
+# this line of code will take an input using the input() function
+
+# print("hello " + input("what is your name?") +"!") output - what is your name - shihab
+#                                                                 hello shihab
+
+
+# variables
+# name= "jack"
+# print(name)
+
+# name="shihab"
+# print(name)
+
+# for finding length of strings --use (len) f
+
+# print(len(input("what is your name?")))
+
+
+# ** i have just turned (line21 )to very,very short line into three very long lines.
+# username =input("what is your name?")
+# length= len(username)
+# print(length)
+# so i was confused that what is happening and what going to be output here
+# ANS- i put the username as 'variable' and gave 'input' f. to print the length of the input from user. THATS SET.
+
+# MY FIRST PRACTICE BY MYSELF
+# one=input("what is your name?")
+# print("ok "+ one + " !!")
+# two=input("how are you!!")
+# print("nice to hear ")
+# three=input("So tell me your qualification..")
+# print("Awesome")
+# four=input("so what is goal by doing coding")
+# print("Ok understood")
+# five=input("how much coding knowledge you have")
+# input("beginner"+" " "intermediate"+ " " "or" + " " "experience"+" " "Ans->")
+# print("ok thats great"+"!!")
+
+# NAMING VARIABLES
+# Rules for variable names
+# Must start with a letter (a-z, A-Z)or underscore_
+# can contain letters, numbers, and underscores only
+# can't start with a number
+# can't use python keywords like class, if else, for,etc.
+# python is case-sensitive(Name not equalls to name)
+
+# final project of this lesson
+
+# BAND NAME GENERATOR
+
+# print("Welcome to the band Name Generator.")
+
+# lets1 = input("Whats the name of the city\n you grew up in"+" ""?")
+
+# lets2=input("What's your pet's name?\n")
+# print("your band could be:" + lets1 +" " + lets2)
+
+
+# num="100"
+# num= int(num)
+# print(num + 50)
+
+
+# name=input("what is your name? ")
+
+# # say hello to user
+# print("hello, "+ name)
+
+
+# important
+
+
+# print("hello"+"shihab") #2 to string add hoche
+
+# print("hello" " " "shihab") 
+
+# print("hello " "shihab") 
+
+# print("hello","shihab") 
+# print("hello"+" " "shihab") #same as 1st one
+
+
+# print("hello " + input("what is your name ?")+" !")
+
+
+# how to find length
+# username= input("what is your name :")
+# length = len(username)
+# print(length)
+
+
+# print("i", "am", "learning", sep="-")
 
