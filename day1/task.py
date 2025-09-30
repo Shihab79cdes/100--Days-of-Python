30cf7881ddfcf555958db445281365ddda6c1442
Shihab79cdes <shihabmallick02@gmail.com>
2025-09-30 10:40:45 +0530

    task.py

--- day1/task.py
+++ day1/task.py
@@ -1,1 +1,8 @@
+# BAND NAME GENERATOR
 
+print("Welcome to the band Name Generator.")
+
+lets1 = input("Whats the name of the city\n you grew up in"+" ""?")
+
+lets2=input("What's your pet's name?\n")
+print("your band could be:" + lets1 +" " + lets2)
