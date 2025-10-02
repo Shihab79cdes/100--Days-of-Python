8d39055218f31a9be704c4678589aede421afe29
Shihab79cdes <shihabmallick02@gmail.com>
2025-10-02 22:04:36 +0530

    Update rock_paper_scissor.py

--- day4/rock_paper_scissor.py
+++ day4/rock_paper_scissor.py
@@ -1,55 +1,56 @@
+
 import random
-rock = '''
-     _______
- ---'   ____)
-       (_____)
-       (_____)
-       (____)
- ---.__(___)
- '''
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+rock = """
+       _______
+   ---'   ____)
+         (_____)
+         (_____)
+         (____)
+   ---.__(___)
+   """
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-images = [rock ,paper ,scissors]
+paper = """
+      _______
+  ---'   ____)____
+            ______)
+            _______)
+           _______)
+  ---.__________)
+ """
 
-user_choice = int(input("what do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissor: "))
+scissor = """
+      _______
+  ---'   ____)____
+            ______)
+         __________)
+        (____)
+  ---.__(___)
 
-if user_choice >=0 and user_choice<=2:
-    print(images[user_choice])
+  """
+user_choice = int(input("What do you chooose ? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+
+option = [rock, paper, scissor]
+
 
 computer_choice = random.randint(0,2)
-print(f"computer chose:")
-print(images[computer_choice])
+
+print(f"you choose:{option[user_choice]}")
+print(f"computer choose :{option[computer_choice]}")
 
 
-if user_choice >=3 or user_choice < 0 :
-    print("invalid number")
-
-elif user_choice==0 and computer_choice ==2:
-    print("you win!")
-elif computer_choice==0 and user_choice==2:
-    print("you lose")
-
-
-elif computer_choice > user_choice :
-    print("you lose!")
-elif user_choice > computer_choice:
-    print("you win")
-
-elif computer_choice== user_choice:
+if user_choice==computer_choice:
     print("its draw")
+
+elif user_choice ==0 and computer_choice==2:
+    print("you won")
+
+
+elif user_choice==1 and computer_choice==0:
+    print("you won")
+
+elif user_choice==2 and computer_choice==1:
+    print("you won")
+else:
+    print("you lose")
